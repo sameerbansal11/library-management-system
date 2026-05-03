f31e76a85efb461f50db7a6ce642205a351588df
Sameer Bansal <sameerbansal2005@gmail.com>
2026-05-03 09:29:43 +0530

    feat: Add initial project structure and main script

---
+++ main.py
@@ -0,0 +1,11 @@
+# library-management-system
+# Author: Sameer Bansal
+# SRM Institute of Science and Technology
+
+def main():
+    print("Library Management System built with Python")
+    print("Project: library-management-system")
+    print("Author: Sameer Bansal | RA2311032010061")
+
+if __name__ == "__main__":
+    main()
